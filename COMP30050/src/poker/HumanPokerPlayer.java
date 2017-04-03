b7e03029abb4954a65e5c24d79cb26ee520666c8
Alan Holmes <alan.holmes@ucdconnect.ie>
2017-04-03 21:48:36 +0100

    Added both classes i will be working on on this sprint

---
+++ COMP30050/src/poker/HumanPokerPlayer.java
@@ -0,0 +1,10 @@
+package poker;
+
+public class HumanPokerPlayer extends PokerPlayer {
+
+	public HumanPokerPlayer(DeckOfCards inputDeck) throws InterruptedException {
+		super(inputDeck);
+		// TODO Auto-generated constructor stub
+	}
+
+}
