9edda1ce21e096c749b125f5dbed83ffae041445
Alan Halvey <Alan.Halvey@ucdconnect.ie>
2017-04-04 13:27:16 +0100

    Added GameOfPoker class.

---
+++ COMP30050/src/poker/GameOfPoker.java
@@ -0,0 +1,26 @@
+package poker;
+
+import java.util.ArrayList;
+
+public class GameOfPoker {
+
+	public static void main(String[] args) throws InterruptedException {
+		DeckOfCards deck = new DeckOfCards();
+		OutputTerminal console = new OutputTerminal();
+		int ante = 1;
+		
+		ArrayList<PokerPlayer> players = new ArrayList<PokerPlayer>(6);
+		PokerPlayer humanPlayer = new HumanPokerPlayer(deck);
+		players.add(humanPlayer);
+		
+		for(int i=0;i<5;i++){
+			PokerPlayer computerPlayer = new AutomatedPokerPlayer(deck);
+			players.add(computerPlayer);			
+		}
+		
+		//HandOfPoker(players, ante, console)
+		
+		
+	}
+
+}
