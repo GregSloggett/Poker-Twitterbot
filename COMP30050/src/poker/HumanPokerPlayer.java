5b082bc90022bff7e335c2a5c3cf3c8086144ac3
Alan Holmes <alan.holmes@ucdconnect.ie>
2017-04-04 15:36:54 +0100

    Fold is completed

--- COMP30050/src/poker/HumanPokerPlayer.java
+++ COMP30050/src/poker/HumanPokerPlayer.java
@@ -7,10 +7,38 @@ public class HumanPokerPlayer extends PokerPlayer {
 		// TODO Auto-generated constructor stub
 	}
 	
-	OutputTerminal output = 
+	OutputTerminal output = new OutputTerminal();
 	
 	public boolean Fold(){
+		String positiveResponse = "y";
+		String negativeResponse = "n";
 		
+		boolean isFold = false;
+		
+		output.printout("Do you want to fold??\n If so type Y for yes or N for no");
+		String Answer = output.readIn();
+		
+		if(Answer.equalsIgnoreCase(positiveResponse)){
+			isFold = true;
+		}
+		else if(Answer.equalsIgnoreCase(negativeResponse)){
+			isFold = false;
+		}
+		else{
+			output.printout("Sorry I didnt regcognise this response");
+			this.Fold();
+		}
+		System.out.println(isFold);
+		
+		return isFold;
+	}
+	
+	public static void main(String[] args) throws InterruptedException{
+		
+		DeckOfCards deck = new DeckOfCards();
+		HumanPokerPlayer human = new HumanPokerPlayer(deck);
+		
+		human.Fold();
 		
 	}
 
