c565ba7cb72d7ab7151b3ebf05f35e10046ceb8a
Alan Holmes <alan.holmes@ucdconnect.ie>
2017-04-05 10:40:51 +0100

    pushed again

--- COMP30050/src/poker/HumanPokerPlayer.java
+++ COMP30050/src/poker/HumanPokerPlayer.java
@@ -1,45 +1,106 @@
 package poker;
 
+import java.util.ArrayList;
+
 public class HumanPokerPlayer extends PokerPlayer {
 
 	public HumanPokerPlayer(DeckOfCards inputDeck) throws InterruptedException {
 		super(inputDeck);
 		// TODO Auto-generated constructor stub
 	}
+
+	
 	
 	OutputTerminal output = new OutputTerminal();
 	
-	public boolean Fold(){
+	
+
+	public void discard() throws InterruptedException {
 		String positiveResponse = "y";
 		String negativeResponse = "n";
-		
+		System.out.println("hello alan");
+
+		output.printout("Do you want to replace some of your cards??\n If so type Y for yes or N for no");
+		String Answer = output.readInString();
+
+		if (Answer.equalsIgnoreCase(positiveResponse)) {
+			output.printout("OK how many cards do you need to change you can discard up to 3 cards");
+			int amountToDiscard = output.readInSingleInt();
+
+			if (amountToDiscard == 1) {
+				output.printout("which card do you want to discard? 1 is the first card up to 5 the rightmost card");
+				int discardedCard = output.readInSingleInt();
+				if (discardedCard > 0 && discardedCard <= 5) {
+					this.hand.replaceCardFromDeck(discardedCard - 1);
+				} else {
+					output.printout("Sorry this isnt a valid card..");
+					this.discard();
+				}
+
+			}else if(amountToDiscard == 2 || amountToDiscard == 3 ){
+				output.printout("which cards do you want to discard? 1 is the first card up to 5 the rightmost card ");
+				ArrayList<Integer> discardedCard = new ArrayList<Integer>();
+				int cardcheck = 0;
+				int[] discarded = new int[amountToDiscard];
+				for(int i =0; i<amountToDiscard;i++){
+					discarded[i] = output.readInSingleInt();
+					if(discarded[i] >0 && discarded[i] <=5){
+						cardcheck+=1;
+					}
+				}
+				if(cardcheck == amountToDiscard){
+					for(int i = 0; i<amountToDiscard; i++){
+						this.hand.replaceCardFromDeck(discarded[i]-1);
+					}
+					
+				}else{
+					output.printout("Sorry one of the card positions you entered is invalid");
+					this.discard();
+					
+				}
+				
+			}else{
+				output.printout("Sorry you can only remove between 1 and 3 cards");
+				this.discard();
+			}
+		}else if(Answer.equalsIgnoreCase(negativeResponse)){
+			output.printout("OK lets continue...");
+		}
+	}
+
+	public boolean Fold() {
+		String positiveResponse = "y";
+		String negativeResponse = "n";
+
 		boolean isFold = false;
-		
+
 		output.printout("Do you want to fold??\n If so type Y for yes or N for no");
-		String Answer = output.readIn();
-		
-		if(Answer.equalsIgnoreCase(positiveResponse)){
+		String Answer = output.readInString();
+
+		if (Answer.equalsIgnoreCase(positiveResponse)) {
 			isFold = true;
-		}
-		else if(Answer.equalsIgnoreCase(negativeResponse)){
+		} else if (Answer.equalsIgnoreCase(negativeResponse)) {
 			isFold = false;
-		}
-		else{
+		} else {
 			output.printout("Sorry I didnt regcognise this response");
 			this.Fold();
 		}
 		System.out.println(isFold);
-		
+
 		return isFold;
 	}
-	
-	public static void main(String[] args) throws InterruptedException{
-		
+
+	public static void main(String[] args) throws InterruptedException {
+
 		DeckOfCards deck = new DeckOfCards();
 		HumanPokerPlayer human = new HumanPokerPlayer(deck);
-		
+
+		System.out.println(human.hand);
+		human.discard();
+		System.out.println(human.hand);
+
 		human.Fold();
-		
+
 	}
 
 }
--- COMP30050/src/poker/OutputTerminal.java
+++ COMP30050/src/poker/OutputTerminal.java
@@ -1,20 +1,48 @@
 package poker;
 
+import java.awt.List;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class OutputTerminal {
 	
+	
+	
 	public void printout(String Output){
 		System.out.println(Output);
+		System.out.println("hello alan");
 		
 	}
 	
-	public String readIn(){
+	public String readInString(){
 		Scanner reader = new Scanner(System.in);
 		
 		String input = reader.next();
 		
 		return input;
 	}
+	
+	public int readInSingleInt(){
+		Scanner reader = new Scanner(System.in);
+		
+		int input = reader.nextInt();
+		
+		return input;
+	}
 
+	public int[] readinMultipleInt(){
+		Scanner reader = new Scanner(System.in);
+
+		
+		String input = reader.nextLine();
+		String[] numberStr= input.split(" ");
+		
+		int[] numbers = new int[numberStr.length];
+		
+		for(int i=0;i<numberStr.length;i++){
+			numbers[i] = Integer.parseInt(numberStr[i]);
+		}
+		return numbers;
+	}
+	
 }
