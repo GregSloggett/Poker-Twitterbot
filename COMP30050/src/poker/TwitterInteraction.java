95559b35357e822050fdcf9155b6f9d85af97d3a
Alan Halvey <Alan.Halvey@ucdconnect.ie>
2017-04-07 10:26:01 +0100

    You can now leave and join games using #FOAKDeal and #FOAKLeave respectively.

--- COMP30050/src/poker/TwitterInteraction.java
+++ COMP30050/src/poker/TwitterInteraction.java
@@ -3,6 +3,8 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.imageio.ImageIO;
 
@@ -10,7 +12,7 @@ import twitter4j.*;
 
 public class TwitterInteraction {
 	static Twitter twitter = TwitterFactory.getSingleton();
-
+	static Map<String, Boolean> usersPlayingGames = new HashMap<String, Boolean>();
 	/**
 	 * This method is just for displaying a String as a status.
 	 * @param status
@@ -33,6 +35,9 @@ public class TwitterInteraction {
 		twitter.updateStatus(status);
 	}
 
+
+
+
 	/**
 	 * When you run this it starts listening
 	 * out for the hashtag #FOAKDeal. When
@@ -42,77 +47,124 @@ public class TwitterInteraction {
 
 	private static void StartHashtagStream() {
 		TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
-
 		StatusListener statusListener = new StatusListener() {
 
 			@Override
 			//This is what happens when a status with our hashtag is detected
 			public void onStatus(Status status) {
-				try {
-					StatusUpdate replyStatus = new StatusUpdate("You have posted our hashtag to play poker.");
-					replyStatus.setInReplyToStatusId(status.getId());
-					twitter.updateStatus(replyStatus);
-				} catch (TwitterException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+
+				try {			
+					if(status.getText().contains("#FOAKDeal")){
+							if(!(usersPlayingGames.containsKey(status.getUser().getScreenName()))){
+								System.out.println("1");
+								System.out.println(status.getUser().getScreenName());
+
+								StatusUpdate replyStatus = new StatusUpdate("You have posted our hashtag to play poker.");
+								replyStatus.setInReplyToStatusId(status.getId());
+								twitter.updateStatus(replyStatus);
+								System.out.println("2");
+								usersPlayingGames.put(status.getUser().getScreenName(), true);
+								System.out.println("3");
+								System.out.println(usersPlayingGames.containsKey(status.getUser().getScreenName()));
+							}
+							else{
+								System.out.println("4");
+								StatusUpdate replyStatus = new StatusUpdate("You're already playing a running game.");
+								System.out.println("5");
+								replyStatus.setInReplyToStatusId(status.getId());
+								System.out.println("6");
+								twitter.updateStatus(replyStatus);
+								System.out.println("7");
+							}
 				}
+					else if(status.getText().contains("#FOAKLeave")){
+						if((usersPlayingGames.containsKey(status.getUser().getScreenName()))){
+							System.out.println("1");
+							System.out.println(status.getUser().getScreenName());
+
+							StatusUpdate replyStatus = new StatusUpdate("You have posted the hashtag to leave a poker game. Thanks for playing!");
+							replyStatus.setInReplyToStatusId(status.getId());
+							twitter.updateStatus(replyStatus);
+							System.out.println("2");
+							usersPlayingGames.remove(status.getUser().getScreenName());
+							System.out.println("3");
+						}
+						else{
+							System.out.println("4");
+							StatusUpdate replyStatus = new StatusUpdate("You're aren't currently playing a poker game. To start a new game post a tweet with the hashtag 'FOAKDeal'");
+							System.out.println("5");
+							replyStatus.setInReplyToStatusId(status.getId());
+							System.out.println("6");
+							twitter.updateStatus(replyStatus);
+							System.out.println("7");
+						}
 			}
 
-			//The below methods are just auto-generated ones that we don't need right now.
-			@Override
-			public void onDeletionNotice(StatusDeletionNotice sdn) {
-				throw new UnsupportedOperationException("Not supported yet."); 
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-
-			@Override
-			public void onTrackLimitationNotice(int i) {
-				throw new UnsupportedOperationException("Not supported yet."); 
-			}
-
-			@Override
-			public void onScrubGeo(long l, long l1) {
-				throw new UnsupportedOperationException("Not supported yet."); 
-			}
-
-			@Override
-			public void onStallWarning(StallWarning sw) {
-				throw new UnsupportedOperationException("Not supported yet.");
-			}
-
-			@Override
-			public void onException(Exception ex) {
-			}
-		};
-
-		FilterQuery filter = new FilterQuery();        
-
-		//These are the keywords it listens out for.
-		String keywords[] = {"FOAKDeal", "#FOAKDeal"};
-
-		filter.track(keywords);        
-
-		twitterStream.addListener(statusListener);
-		twitterStream.filter(filter);          
-	}  
-
-	/**
-	 * Testing updating status with text with an image below it.
-	 * I just loaded from URL so that I didn't have to load any 
-	 * images into the project, it'd be the same process for an image
-	 * loaded from file.
-	 */
-	private void testStatusWithTextAndImage() throws IOException, TwitterException{
-		URL url = new URL("https://theroommom.files.wordpress.com/2013/07/a-hand-of-cards.jpg");
-		BufferedImage image = ImageIO.read(url);
-		File file = new File("image.jpg");
-		ImageIO.write(image,"jpg",file);
-
-		updateStatusWithTextAndImage("This is a hand of cards",file);
-	}
+		}
 
 
-	public static void main(String[] args) throws TwitterException, IOException {
-		StartHashtagStream();
-	}
+		//The below methods are just auto-generated ones that we don't need right now.
+		@Override
+		public void onDeletionNotice(StatusDeletionNotice sdn) {
+			throw new UnsupportedOperationException("Not supported yet."); 
+		}
+
+		@Override
+		public void onTrackLimitationNotice(int i) {
+			throw new UnsupportedOperationException("Not supported yet."); 
+		}
+
+		@Override
+		public void onScrubGeo(long l, long l1) {
+			throw new UnsupportedOperationException("Not supported yet."); 
+		}
+
+		@Override
+		public void onStallWarning(StallWarning sw) {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
+
+		@Override
+		public void onException(Exception ex) {
+		}
+	};
+
+	FilterQuery filter = new FilterQuery();        
+
+	//These are the keywords it listens out for.
+	String keywords[] = {"#FOAKDeal","#FOAKLeave"};
+
+	filter.track(keywords);        
+
+	twitterStream.addListener(statusListener);
+	twitterStream.filter(filter);          
+}  
+
+
+
+/**
+ * Testing updating status with text with an image below it.
+ * I just loaded from URL so that I didn't have to load any 
+ * images into the project, it'd be the same process for an image
+ * loaded from file.
+ */
+private static void testStatusWithTextAndImage() throws IOException, TwitterException{
+	URL url = new URL("https://www.drupal.org/files/issues/sample_7.png");
+	BufferedImage image = ImageIO.read(url);
+	File file = new File("image.png");
+	ImageIO.write(image,"png",file);
+
+	updateStatusWithTextAndImage("Testing png",file);
+}
+
+
+public static void main(String[] args) throws TwitterException, IOException {
+	StartHashtagStream();
+	//testStatusWithTextAndImage();
+}
 
 }
